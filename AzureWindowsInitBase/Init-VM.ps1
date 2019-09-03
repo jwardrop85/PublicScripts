a698e5c005d9d999bac3426d6c2a7e8b5c1b77f3
John Wardrop <johnwardrop@outlook.com>
2019-09-03 15:16:39 +0100

    Format disks

--- AzureWindowsInitBase/Init-VM.ps1
+++ AzureWindowsInitBase/Init-VM.ps1
@@ -4,5 +4,18 @@ Set-WinHomeLocation -GeoId 242
 Set-WinUserLanguageList -LanguageList (New-WinUserLanguageList -Language en-GB) -Force
 Set-Culture en-GB
 Start-Sleep -Seconds 40
+$currentDriveLetters=Get-Volume | Where-Object{$_.DriveLetter} | Select -ExpandProperty DriveLetter
+$lastDriveLetterChar=([int[]][char[]]$nextdisk | Measure -Maximum | select -ExpandProperty Maximum)
+Get-Disk | Where-Object{$_.IsOffline -eq $true} | Set-Disk -IsOffline $false
+$disks=Get-Disk | Where-Object{$_.PartitionStyle -eq "RAW"}
+$disks | Initialize-Disk -PartitionStyle GPT
+foreach($disk in $disks)
+{
+    $lastDriveLetterChar = $lastDriveLetterChar + 1
+    $disk | New-Partition -UseMaximumSize -DriveLetter "$([char][int]$lastDriveLetterChar)"
+    Format-Volume -DriveLetter "$([char][int]$lastDriveLetterChar)" -FileSystem "NTFS" -Full
+}
+
+
 Restart-Computer
 exit 0
