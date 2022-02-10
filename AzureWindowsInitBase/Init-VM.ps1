3503076a222da4a2ba0a84a480f35f00f0a3ef8e
jwardrop85 <49334517+jwardrop85@users.noreply.github.com>
2022-02-10 14:18:10 +0000

    Update Init-VM.ps1

--- AzureWindowsInitBase/Init-VM.ps1
+++ AzureWindowsInitBase/Init-VM.ps1
@@ -7,16 +7,16 @@ Start-Sleep -Seconds 40
 mkdir C:\windows\system32\config\systemprofile\AppData\Local\Temp\
 Copy-Item ./securitypolicy.inf -Destination C:\windows\system32\config\systemprofile\AppData\Local\Temp\ -Force
 Start-sleep -Seconds 10
-$currentDriveLetters=Get-Volume | Where-Object{$_.DriveLetter} | Select -ExpandProperty DriveLetter
-$lastDriveLetterChar=([int[]][char[]]$currentDriveLetters | Measure -Maximum | select -ExpandProperty Maximum)
-Get-Disk | Where-Object{$_.IsOffline -eq $true} | Set-Disk -IsOffline $false
-$disks=Get-Disk | Where-Object{$_.PartitionStyle -eq "RAW"}
-$disks | Initialize-Disk -PartitionStyle GPT
-foreach($disk in $disks)
-{
-    $lastDriveLetterChar = $lastDriveLetterChar + 1
-    $disk | New-Partition -UseMaximumSize -DriveLetter "$([char][int]$lastDriveLetterChar)"
-    Format-Volume -DriveLetter "$([char][int]$lastDriveLetterChar)" -FileSystem "NTFS" -Full
-}
+#$currentDriveLetters=Get-Volume | Where-Object{$_.DriveLetter} | Select -ExpandProperty DriveLetter
+#$lastDriveLetterChar=([int[]][char[]]$currentDriveLetters | Measure -Maximum | select -ExpandProperty Maximum)
+#Get-Disk | Where-Object{$_.IsOffline -eq $true} | Set-Disk -IsOffline $false
+#$disks=Get-Disk | Where-Object{$_.PartitionStyle -eq "RAW"}
+#$disks | Initialize-Disk -PartitionStyle GPT
+#foreach($disk in $disks)
+#{
+#    $lastDriveLetterChar = $lastDriveLetterChar + 1
+#    $disk | New-Partition -UseMaximumSize -DriveLetter "$([char][int]$lastDriveLetterChar)"
+#    Format-Volume -DriveLetter "$([char][int]$lastDriveLetterChar)" -FileSystem "NTFS" -Full
+#}
 Restart-Computer
 exit 0
